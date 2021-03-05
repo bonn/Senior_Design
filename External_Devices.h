057d09ae0a18f050bd08556f010fecb34e6aa6b6
Jordan Bonn <bonn.jordan@gmail.com>
2021-03-05 17:13:31 -0600

    Update External_Devices.h

--- External_Devices.h
+++ External_Devices.h
@@ -1,15 +1,22 @@
 
 //Jordan Bonn
 
-#include <iostream>
-#include <utility>
-#include <set>
-#include <vector>
+#include <multi_channel_relay.h>
+
+  /** 
+   *  channle: 4 3 2 1
+   *  state: 0b0000 -> 0x00  (all off)
+   *  state: 0b1111 -> 0x0f   (all on)
+  */ 
+
+Multi_Channel_Relay relay;
 
 //This class is used to create the pitchers and then has functions to add and remove water from the buckets
 class External_Devices {
 public:
     
+    relay.begin(0x11); 
+
     External_Devices() {
         
     }
@@ -18,20 +25,37 @@ public:
     bool Dev_1,Dev_2,Dev_3,Dev_4;
 
     float desired_light_level;
+    int device;
 
 
+//This function is called to turn off a device connected to the relay.
+//It is passed an int between 1-4 and turns off the appropiate device
+    void turn_off_dev(device){
 
-    float get_AirTemp(){
+        relay.turn_off_channel(device);
 
-
-        return Air_Temp;
+        return;
     }
 
-    float get_AirHum(){
+//This function is called to turn on a device connected to the relay.
+//It is passed an int between 1-4 and turns off the appropiate device
+    void turn_on_dev(device){
 
-        return Air_Humidity;
+        relay.turn_on_channel(device);
+
+        return;
     }
 
+//This function is called to turn on a device connected to the relay.
+//It is passed an int between 1-4 and turns off the appropiate device
+    void get_relay_status(device){
+
+        relay.turn_on_channel(device);
+
+        return;
+    }
+
+    
     uint8_t get_LightLevel_1(){
 
         return Light_1;
