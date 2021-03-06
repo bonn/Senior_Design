8febd5906994b059c05e8ed5654f3ea72664e7e0
bonn <bonn.jordan@gmail.com>
2021-03-05 18:23:05 -0600

    Update External_Devices.h

--- External_Devices.h
+++ External_Devices.h
@@ -3,29 +3,30 @@
 
 #include <multi_channel_relay.h>
 
-  /** 
+#include <Wire.h>
+#include <Max517Dac.h>
+
+  /**
    *  channle: 4 3 2 1
    *  state: 0b0000 -> 0x00  (all off)
    *  state: 0b1111 -> 0x0f   (all on)
-  */ 
+  */
 
 Multi_Channel_Relay relay;
+Max517Dac   dac;
 
 //This class is used to create the pitchers and then has functions to add and remove water from the buckets
 class External_Devices {
 public:
-    
-    relay.begin(0x11); 
+
+    relay.begin(0x11);
 
     External_Devices() {
-        
-    }
-    
-    
-    bool Dev_1,Dev_2,Dev_3,Dev_4;
 
-    float desired_light_level;
-    int device;
+    }
+
+    bool Dev_1,Dev_2,Dev_3,Dev_4;
+    uint8_t device,desired_light_level;
 
 
 //This function is called to turn off a device connected to the relay.
@@ -46,48 +47,68 @@ public:
         return;
     }
 
-//This function is called to turn on a device connected to the relay.
-//It is passed an int between 1-4 and turns off the appropiate device
-    void get_relay_status(device){
+//This function is called to return the current state of the relay
+    bool get_relay_status(device){
 
-        relay.turn_on_channel(device);
+        switch (device){
+
+           case 1:
+           return Dev_1;
+
+           case 2:
+           return Dev_2;
+
+           case 3:
+           return Dev_3;
+
+           case 4:
+           return Dev_4;
+
+           default:
+           return Dev_1;
+      }
+
+    }
+
+//This function is called to set the dimemr level
+//A value of 0 will be the lowest light ( hopefuly off)
+//A valuse of 255 will turn the lights all the way up
+    void set_light_level(desired_light_level){
+
+        if (!dac.setOutput(desired_light_level)){
+
+           Serial.println("Error talking to DAC. Check wiring.");
+        }
 
         return;
     }
+//This function is called to return the current value of the dimmer
+//This can be used to show the current value on the GUI
+    uint8_t get_light_level(){
 
-    
-    uint8_t get_LightLevel_1(){
-
-        return Light_1;
+        return desired_light_level;
     }
 
-    
-};
+}
 
 
 int main()
 
 {
-    int x, y, z;
-    
-    std::cout << "Please enter the size of the first pitcher." << '\n';
-    
-    std::cin >> x;
-    
-    std::cout << "Please enter the size of the second pitcher." << '\n';
-    
-    std::cin >> y;
-    
-    std::cout << "Please enter the requested amount of water." << '\n';
-    
-    std::cin >> z;
-    
-    
-    Pitchers pitchers;
-    
-    pitchers.inputBucketSizes(x, y);
-    pitchers.inputDesiredAmount(z);
-    pitchers.getDesiredResult_Depth();
-    
+
+
+
+    External_Devices external;
+
+    external.turn_on_dev(1);
+
+    external.get_relay_status(1);
+
+    external.turn_off_dev(1)
+
+    external.set_light_level(255);
+
+    external.get_light_level();
+
     return 0;
 }
