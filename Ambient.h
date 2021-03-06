51dd30175517a34b4ba08357299dac5f5127ac30
bonn <bonn.jordan@gmail.com>
2021-03-05 19:35:42 -0600

    Update Ambient.h

--- Ambient.h
+++ Ambient.h
@@ -1,73 +1,91 @@
 
 //Jordan Bonn
 
-#include <iostream>
-#include <utility>
-#include <set>
-#include <vector>
+#include <ESP8266WiFi.h>                //ESP8266 Library
+
+#include <Wire.h>
+#include "SparkFun_VEML6030_Ambient_Light_Sensor.h"
+#include "DHTesp.h"
+
+#define AL_ADDR_1 0x48
+#define AL_ADDR_2 0x10
+
+//Creating the device for the VEML sensor; we will use 2 sensors
+SparkFun_Ambient_Light light_sens_1(AL_ADDR_1);
+SparkFun_Ambient_Light light_sens_2(AL_ADDR_2);
+
+// Possible values: .125, .25, 1, 2
+// Both .125 and .25 should be used in most cases except darker rooms.
+// A gain of 2 should only be used if the sensor will be covered by a dark
+// glass.
+float gain = .125;
+
+// Possible integration times in milliseconds: 800, 400, 200, 100, 50, 25
+// Higher times give higher resolutions and should be used in darker light.
+int time_1 = 100;
+long luxVal = 0;
+
+//Creating the DHT device
+DHTesp dht;
+
 
 //This class is used to create the pitchers and then has functions to add and remove water from the buckets
 class Ambient {
 public:
-    
+
+    Wire.begin();
+    light_sens_1.begin();
+    light_sens_1.setGain(gain);
+    light_sens_1.setIntegTime(time_1);
+
+    light_sens_2.begin();
+    light_sens_2.setGain(gain);
+    light_sens_2.setIntegTime(time_1);
+
+    dht.setup(10, DHTesp::DHT22); // Connect DHT sensor to GPIO 10
+
     Ambient() {
-        
-    }
-    
- 
-    float Air_Temp, Air_Humidity;
 
-    uint8_t Light_1,Light_2
-
-
-
-    float get_AirTemp(){
-
-
-        return Air_Temp;
     }
 
-    float get_AirHum(){
+// This is used to select sensor 1 or 2
+// sensor 1 is false (0) and sensor 2 is true (1)
+    bool light_sensor;
 
-        return Air_Humidity;
+//This function will return the temp in Farenheit
+    float get_temp(){
+//        Air_Temp = dht.getTemperature();
+        return dht.toFahrenheit(dht.getTemperature());
     }
 
-    uint8_t get_LightLevel_1(){
-
-        return Light_1;
+//This function will return the humidity as a percentage
+    float get_humidity(){
+//        Air_Humidity = dht.getHumidity();
+        return dht.getHumidity();
     }
 
-    uint8_t get_LightLevel_2(){
+//This functin will return the light sensor read
+//It takes a bool arg where false will return sensor 1 data and
+    uint8_t get_light_reading(light_sensor){
 
-        return Light_2;
+      if(!light_sensor){
+        return light_sens_1.readLight();
+      }
+        return light_sens_2.readLight();
     }
-    
+
 };
 
 
 int main()
-
 {
-    int x, y, z;
-    
-    std::cout << "Please enter the size of the first pitcher." << '\n';
-    
-    std::cin >> x;
-    
-    std::cout << "Please enter the size of the second pitcher." << '\n';
-    
-    std::cin >> y;
-    
-    std::cout << "Please enter the requested amount of water." << '\n';
-    
-    std::cin >> z;
-    
-    
-    Pitchers pitchers;
-    
-    pitchers.inputBucketSizes(x, y);
-    pitchers.inputDesiredAmount(z);
-    pitchers.getDesiredResult_Depth();
-    
+    Ambient ambient;
+
+    ambient.get_temp();
+    ambient.get_humidity();
+    ambient.get_light_reading(0);
+    ambient.get_light_reading(1);
+
+
     return 0;
 }
